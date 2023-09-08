7111d7e492f59e6dce549541ec210eada65b9ebb
itsabdull15 <itsabdullah.cg@gmail.com>
2023-09-09 03:04:55 +0530

    django projects

---
+++ UdemyDjangoWebApp/src/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import login_view, RegisterView, logout_view
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', logout_view, name='logout'),
+
+]
